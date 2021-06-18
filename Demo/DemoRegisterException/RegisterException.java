76ee04ee516cb9bf8051bf525a85f976e1df626f
Frank-gg <897938948@qq.com>
2021-06-18 17:24:30 +0800

    Exception exercise.

---
+++ Demo/DemoRegisterException/RegisterException.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class RegisterException extends Exception{
+
+    public RegisterException(){
+        super();
+    }
+
+    public RegisterException(String message){
+        super(message);
+    }
+}
