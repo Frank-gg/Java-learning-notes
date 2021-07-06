0cbd168fac4f2517ea9987ea88f5deb675e36340
Frank-gg <897938948@qq.com>
2021-07-06 22:37:45 +0800

    Reflection exercise.

---
+++ Demo/DemoReflection/Person.java
@@ -0,0 +1,54 @@
+package DemoReflection;
+
+
+
+public class Person {
+
+    private String name;
+    public int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void show(){
+        System.out.println("我是你爹！");
+    }
+
+    private String showNation(String nation){
+        System.out.println("我是中国人！" + nation);
+        return nation;
+    }
+}
